8fa9f2ac9a13ad8c1fdbd6cb72f309341b7b90d4
Aytaj Gahramanova <132328862+AytajGahramanova@users.noreply.github.com>
2025-10-05 13:38:29 +0400

    feat:product-details

--- assets/script.js
+++ assets/script.js
@@ -50,6 +50,27 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+
+// product-details swiper
+// var swiper = new Swiper(".detailSwiper", {
+//   loop: true,
+//   spaceBetween: 10,
+//   slidesPerView: 4,
+//   freeMode: true,
+//   watchSlidesProgress: true,
+// });
+// var swiper2 = new Swiper(".detailSwiper2", {
+//   loop: true,
+//   spaceBetween: 10,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   thumbs: {
+//     swiper: swiper,
+//   },
+// });
+
 // fancybox
 Fancybox.bind("[data-fancybox]", {});
 
